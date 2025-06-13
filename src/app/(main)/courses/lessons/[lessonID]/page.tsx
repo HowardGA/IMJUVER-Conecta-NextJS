e320d8f2c402c41fb021f7a79876b246e9fd8fbb
HowardGA <131924292+HowardGA@users.noreply.github.com>
2025-06-13 14:25:46 -0700

    refactor: update SingleLessonPageProps to use a Promise for params handling

--- src/app/(main)/courses/lessons/[lessonID]/page.tsx
+++ src/app/(main)/courses/lessons/[lessonID]/page.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import SingleLessonClientContent from './SingleLessonClientContent';
 
 interface SingleLessonPageProps {
-  params: {
+  params:Promise<{
     lessonID: string; 
-  };
+  }>;
 }
 
 export default async function SingleLessonPage({ params }: SingleLessonPageProps) {
-  const lessonId = parseInt(params.lessonID);
-      console.log(lessonId)
+  const {lessonID} = await params;
 
-  return <SingleLessonClientContent lessonID={lessonId} />;
+  return <SingleLessonClientContent lessonID={parseInt(lessonID)} />;
 }
