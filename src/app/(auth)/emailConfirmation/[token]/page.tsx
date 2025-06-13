86ee191db52114f470c4c71a05df07a73da96776
HowardGA <131924292+HowardGA@users.noreply.github.com>
2025-06-13 14:06:18 -0700

    refactor: update EmailConfirmationPageProps to use a Promise for params handling

--- src/app/(auth)/emailConfirmation/[token]/page.tsx
+++ src/app/(auth)/emailConfirmation/[token]/page.tsx
@@ -1,13 +1,14 @@
 import EmailConfirmationClient from '../../components/EmailConfirmationClient';
 
 interface EmailConfirmationPageProps {
-  params: {
+  params: Promise<{
     token: string;
-  };
+  }>;
 }
 
-const EmailConfirmationPage = ({ params }: EmailConfirmationPageProps) => {
-  const { token } = params;
+const EmailConfirmationPage = async ({ params }: EmailConfirmationPageProps) => { 
+  const { token } = await params;
+
   return <EmailConfirmationClient token={token} />;
 };
 
