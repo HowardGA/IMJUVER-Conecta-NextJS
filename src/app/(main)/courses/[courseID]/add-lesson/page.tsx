61d174319eeff541fb07bbcdece602e876900c4b
HowardGA <131924292+HowardGA@users.noreply.github.com>
2025-06-13 14:13:06 -0700

    refactor: update AddLessonPageProps to use a Promise for params handling

--- src/app/(main)/courses/[courseID]/add-lesson/page.tsx
+++ src/app/(main)/courses/[courseID]/add-lesson/page.tsx
@@ -2,18 +2,13 @@ import React from 'react';
 import AddLessonClient from './AddLessonClient';
 
 interface AddLessonPageProps {
-  params: {
+   params: Promise<{
     courseID: string;
-  };
-}
-
-export default function AddLessonPage({ params }: AddLessonPageProps) {
-  const courseId = parseInt(params.courseID);
-  console.log(courseId)
-
-  if (isNaN(courseId)) {
-    return <p>ID de curso inválido.</p>;
+  }>;
   }
 
-  return <AddLessonClient courseId={courseId} />;
+export default async function  AddLessonPage({ params }: AddLessonPageProps) {
+  const { courseID } = await params;
+
+  return <AddLessonClient courseId={parseInt(courseID)} />;
 }
