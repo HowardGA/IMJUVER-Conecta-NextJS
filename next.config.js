8a4bf8b13887cdf02402653be6c76e6dfb8bb82e
HowardGA <131924292+HowardGA@users.noreply.github.com>
2025-08-28 20:38:04 -0700

    fix: add remote pattern for Dropbox in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ const nextConfig = {
         port: '',
         pathname: '/**', 
       },
+      {
+        protocol: 'https',
+        hostname: 'www.dropbox.com',
+        pathname: '/**', 
+      },
     ],
   },
 };
